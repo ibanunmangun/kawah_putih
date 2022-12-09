2a8d2c8c30f0e29f42fce1b06069dea06bbb4b4c
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-12-09 23:00:32 +0800

    Add files via upload

---
+++ data/KawahPutih_3.js
@@ -0,0 +1,19 @@
+var json_KawahPutih_3 = {
+"type": "FeatureCollection",
+"name": "KawahPutih_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": "1", "Destinatio": "Apache Camp", "Latitude": -7.1677912231610339, "Longitude": 107.40002365941739, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.400023659417386, -7.167791223161033 ] } },
+{ "type": "Feature", "properties": { "No": "2", "Destinatio": "Astrotourism", "Latitude": -7.1677921787736238, "Longitude": 107.40013546609048, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.400135466090475, -7.167792178773623 ] } },
+{ "type": "Feature", "properties": { "No": "3", "Destinatio": "Sunrise Point Sunan Ibu", "Latitude": -7.1678810507445618, "Longitude": 107.40010775332534, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.400107753325344, -7.167881050744561 ] } },
+{ "type": "Feature", "properties": { "No": "4", "Destinatio": "Kawah Putih", "Latitude": -7.166196629, "Longitude": 107.40211282718001, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.402112827180005, -7.166196629 ] } },
+{ "type": "Feature", "properties": { "No": "5", "Destinatio": "Jembatan Apung", "Latitude": -7.166809, "Longitude": 107.401204, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.401204, -7.166809 ] } },
+{ "type": "Feature", "properties": { "No": "6", "Destinatio": "Skywalk Cantigi", "Latitude": -7.166838, "Longitude": 107.404412, "Info": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.404412, -7.166838 ] } },
+{ "type": "Feature", "properties": { "No": "P1", "Destinatio": "Puskesmas RANCABALI", "Latitude": -7.130780583, "Longitude": 107.41992, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.41992, -7.130780583 ] } },
+{ "type": "Feature", "properties": { "No": "P2", "Destinatio": "Polsek Ciwidey", "Latitude": -7.102790218, "Longitude": 107.4648038, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.4648038, -7.102790218 ] } },
+{ "type": "Feature", "properties": { "No": "P3", "Destinatio": "SPBU 34.40909 ciwidey", "Latitude": -7.122154644, "Longitude": 107.4277631, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.4277631, -7.122154644 ] } },
+{ "type": "Feature", "properties": { "No": "P4", "Destinatio": "SPBU 11.1.1.1.11.1 Solo", "Latitude": -7.122154644, "Longitude": 107.4276665, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.4276665, -7.122154644 ] } },
+{ "type": "Feature", "properties": { "No": "P5", "Destinatio": "POS PEMADAM KEBAKARAN CIWIDEY (PACIRA )", "Latitude": -7.104017358, "Longitude": 108.073527107, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 108.073527107, -7.104017358 ] } },
+{ "type": "Feature", "properties": { "No": "P6", "Destinatio": "Parkiran bawah Kawah Putih", "Latitude": -7.142631902, "Longitude": 107.3978635, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.3978635, -7.142631902 ] } }
+]
+}
